16edae6aec1d17ade7500125a23345a0c5e9e2ad
das <mariyadasu.nukathoti@cdk.com>
2019-04-07 20:01:48 +0530

    removed

--- StormDashboard.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StormDashboard.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- StormDashboard.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StormDashboard.Api.WebApiApplication" Language="C#" %>
